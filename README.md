6387a223795a3a64b8a1094b8570266d0016c27f
HYPERION-PC\Professional <hardanger1994@gmail.com>
2020-01-03 20:51:55 +0300

    add WalkDetector.cs

--- README.md
+++ README.md
@@ -3,4 +3,7 @@
 [ChildParticlesScaler.cs](Scripts/ChildParticlesScaler.cs) — changes the scales of child particle systems in accordance with the scale of the object with this script attached to (runtime only). For use in cases where Hierarchy Scaling Mode have no effect.
 
 [ChildLightsScaler.cs](Scripts/ChildLightsScaler.cs) — just like ChildParticlesScaler, but only for Light components.
+
 [GameEvent.cs](Scripts/GameEvent.cs) — event implementation through scriptable object.
+
+[WalkDetector.cs](Scripts/WalkDetector.cs) — determines whether 2 VR Hand Controllers (or pair gameobjects like character hands or legs) move as if walking.
---
+++ Scripts/WalkDetector.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class WalkDetector : MonoBehaviour
+{
+    [SerializeField] GameObject leftHand;
+    [SerializeField] GameObject rightHand;
+
+    public bool IsWalking { get; private set; } = false;
+
+    bool isRightMoving = false;
+    bool isLeftMoving = false;
+
+    float prevLeftY = 0;
+    float prevRightY = 0;
+
+    void Update()
+    {
+        float leftDelta = leftHand.transform.position.y - prevLeftY;
+        float rightDelta = rightHand.transform.position.y - prevRightY;
+
+        if (leftDelta != 0)
+            isLeftMoving = true;
+        else
+            StartCoroutine(LeftStopTimer());
+
+        if (rightDelta != 0)
+            isRightMoving = true;
+        else
+            StartCoroutine(RightStopTimer());
+
+        if (isRightMoving && isLeftMoving && ((leftDelta < 0) != (rightDelta < 0)))
+            IsWalking = true;
+        else
+            IsWalking = false;
+
+        prevLeftY = leftHand.transform.position.y;
+        prevRightY = rightHand.transform.position.y;
+    }
+
+    IEnumerator LeftStopTimer()
+    {
+        float cur = leftHand.transform.position.y;
+        yield return new WaitForSeconds(0.2f);
+        isLeftMoving = cur != leftHand.transform.position.y; 
+    }
+
+    IEnumerator RightStopTimer()
+    {
+        float cur = rightHand.transform.position.y;
+        yield return new WaitForSeconds(0.2f);
+        isRightMoving = cur != rightHand.transform.position.y;
+    }
+}
