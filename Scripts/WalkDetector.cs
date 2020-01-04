a7fd878c4486ea15ae0b764df2fd1825ae6e753c
HYPERION-PC\Professional <hardanger1994@gmail.com>
2020-01-04 12:11:52 +0300

    Update WalkDetector.cs
    some optimization

--- Scripts/WalkDetector.cs
+++ Scripts/WalkDetector.cs
@@ -10,11 +10,12 @@ public class WalkDetector : MonoBehaviour
 
     bool isRightMoving = false;
     bool isLeftMoving = false;
+    bool isTimer = false;
 
     float prevLeftY = 0;
     float prevRightY = 0;
 
-    void Update()
+    void FixedUpdate()
     {
         float leftDelta = leftHand.transform.position.y - prevLeftY;
         float rightDelta = rightHand.transform.position.y - prevRightY;
@@ -26,29 +27,35 @@ public class WalkDetector : MonoBehaviour
 
         if (rightDelta != 0)
             isRightMoving = true;
-        else
+        else if (!isTimer)
             StartCoroutine(RightStopTimer());
 
         if (isRightMoving && isLeftMoving && ((leftDelta < 0) != (rightDelta < 0)))
             IsWalking = true;
-        else
+        else if (!isTimer)
             IsWalking = false;
 
         prevLeftY = leftHand.transform.position.y;
         prevRightY = rightHand.transform.position.y;
+
+        Debug.Log(IsWalking);
     }
 
     IEnumerator LeftStopTimer()
     {
+        isTimer = true;
         float cur = leftHand.transform.position.y;
         yield return new WaitForSeconds(0.2f);
-        isLeftMoving = cur != leftHand.transform.position.y; 
+        isLeftMoving = cur != leftHand.transform.position.y;
+        isTimer = false;
     }
 
     IEnumerator RightStopTimer()
     {
+        isTimer = true;
         float cur = rightHand.transform.position.y;
         yield return new WaitForSeconds(0.2f);
         isRightMoving = cur != rightHand.transform.position.y;
+        isTimer = false;
     }
 }
