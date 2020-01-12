34058b1640c7d5c482978029af4a8f6ce571a06f
HYPERION-PC\Professional <hardanger1994@gmail.com>
2020-01-12 19:58:02 +0300

    Update WalkDetector.cs

--- Scripts/WalkDetector.cs
+++ Scripts/WalkDetector.cs
@@ -5,6 +5,8 @@ public class WalkDetector : MonoBehaviour
 {
     [SerializeField] GameObject leftHand;
     [SerializeField] GameObject rightHand;
+    [Space]
+    [SerializeField] float moveFault = 0.03f;
 
     public bool IsWalking { get; private set; } = false;
 
@@ -17,36 +19,36 @@ public class WalkDetector : MonoBehaviour
 
     void FixedUpdate()
     {
-        float leftDelta = leftHand.transform.position.y - prevLeftY;
-        float rightDelta = rightHand.transform.position.y - prevRightY;
+		float leftDelta = leftHand.transform.position.y - prevLeftY;
+		float rightDelta = rightHand.transform.position.y - prevRightY;
 
-        if (leftDelta != 0)
-            isLeftMoving = true;
-        else
-            StartCoroutine(LeftStopTimer());
+		if (Mathf.Abs(leftDelta) > moveFault)
+			isLeftMoving = true;
+		else if (isLeftMoving && !isTimer)
+			StartCoroutine(LeftStopTimer());
 
-        if (rightDelta != 0)
-            isRightMoving = true;
-        else if (!isTimer)
-            StartCoroutine(RightStopTimer());
+		if (Mathf.Abs(rightDelta) > moveFault)
+			isRightMoving = true;
+		else if (isRightMoving && !isTimer)
+			StartCoroutine(RightStopTimer());
 
-        if (isRightMoving && isLeftMoving && ((leftDelta < 0) != (rightDelta < 0)))
-            IsWalking = true;
-        else if (!isTimer)
-            IsWalking = false;
+		if (isRightMoving && isLeftMoving && ((leftDelta < 0) != (rightDelta < 0)))
+			IsWalking = true;
+		else if (!isTimer)
+			IsWalking = false;
 
-        prevLeftY = leftHand.transform.position.y;
-        prevRightY = rightHand.transform.position.y;
+		prevLeftY = leftHand.transform.position.y;
+		prevRightY = rightHand.transform.position.y;
 
-        Debug.Log(IsWalking);
+		Debug.Log("walking " + IsWalking);
     }
 
     IEnumerator LeftStopTimer()
     {
         isTimer = true;
         float cur = leftHand.transform.position.y;
-        yield return new WaitForSeconds(0.2f);
-        isLeftMoving = cur != leftHand.transform.position.y;
+        yield return new WaitForSeconds(updateInterval);
+        isLeftMoving = Mathf.Abs(cur - leftHand.transform.position.y) > moveFault;
         isTimer = false;
     }
 
@@ -54,8 +56,8 @@ public class WalkDetector : MonoBehaviour
     {
         isTimer = true;
         float cur = rightHand.transform.position.y;
-        yield return new WaitForSeconds(0.2f);
-        isRightMoving = cur != rightHand.transform.position.y;
+        yield return new WaitForSeconds(updateInterval);
+        isRightMoving = Mathf.Abs(cur - rightHand.transform.position.y) > moveFault;
         isTimer = false;
     }
 }
